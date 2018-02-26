ddd13e292db7ec45d1d4a263af13fa912e167a6b
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-02-26 12:41:23 -0800

    problem 2

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -56,7 +56,13 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try {
+      Item.validateName(itemName);
+      store.items.unshift(Item.create(itemName));
+      render();
+    } catch(error) {
+      console.log(`Cannot add item: ${error.message}`);
+    }
   }
   
   function handleNewItemSubmit() {
