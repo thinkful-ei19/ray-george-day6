abc1bd775679d39e1fe2df57c3fc5b798c82a245
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-02-26 08:59:32 -0800

    work in progress first exercise

---
+++ scripts/store.js
@@ -0,0 +1,18 @@
+const store = (function(){
+    {
+        items: [
+          { id: cuid(), name: 'apples', checked: false },
+          { id: cuid(), name: 'oranges', checked: false },
+          { id: cuid(), name: 'milk', checked: true },
+          { id: cuid(), name: 'bread', checked: false }
+        ],
+        hideCheckedItems: false,
+        searchTerm: ''
+    };
+
+    return {
+        items: items,
+        hideCheckedItems: hideCheckedItems,
+        searchTerm: searchTerm
+    };
+}());
